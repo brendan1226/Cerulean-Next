5ba5becd817a6619bbe2c3eecec13ee178bbae9f
brendan1226 <brendan@bywatersolutions.com>
2026-03-11 16:18:04 +0000

    Update g7a2b3c4d5e6_add_koha_auth_type.py

--- alembic/versions/g7a2b3c4d5e6_add_koha_auth_type.py
+++ alembic/versions/g7a2b3c4d5e6_add_koha_auth_type.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "g7a2b3c4d5e6"
-down_revision: Union[str, None] = "f6a1b2c3d4e5"
+down_revision: Union[str, None] = "a1b2c3d4e5f6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
